e57ba5f7cd3acc35819d33ef5ad64ce205c7eef4
Gabe Arnold <gabesarn@gmail.com>
2017-07-07 00:46:33 -0400

    Updated photos, users in our models. Also fixed a small bug in our snapagram.js routes.

--- models/photos.js
+++ models/photos.js
@@ -1,2 +1,23 @@
 const Sequelize = require("sequelize");
 const sql = require("../utility/sql");
+const User = require("./users.js");
+const Likes = require("./likes.js");
+
+
+const Photos = sql.define("photos", {
+	id: {
+		type: Sequelize.INTEGER,
+		primaryKey: true,
+		autoIncrement: true,
+	},
+	user: {
+		type: Sequelize.STRING,
+		notNull: true,
+	},
+});
+
+Photos.hasOne(User);
+Photos.hasMany(Likes);
+
+
+module.exports = Photos;

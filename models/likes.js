fd2d6d7bbc1686cdb572398fadd2e3c144a7f8de
Gabe Arnold <gabesarn@gmail.com>
2017-07-09 14:38:05 -0400

    Updated views templates to include necessary CSS.

--- models/likes.js
+++ models/likes.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize");
 const sql = require("../utility/sql");
-const User = require(".//users.js");
-const Photos = require("../photos.js");
+const User = require("./users.js");
+const Photos = require("./photos.js");
 
 
 const Likes = sql.define("like", {

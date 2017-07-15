e1c169fa3812447f97012b6ddfe906e2001e1bae
Samir Dhebar <samirdhebar@gmail.com>
2017-07-15 19:46:28 -0400

    created initial css. will still need to export files individually for each views page through template function

--- util/renderTemplate.js
+++ util/renderTemplate.js
@@ -1,4 +1,4 @@
-function renderTemplate(res, title, page, args) {
+function renderTemplate(res, title, page, args, links, error) {
 	return res.render("template", {
 		page: page,
 		title: title || "Hey Mom",
